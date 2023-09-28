b2e0d430003f79d1856582c8b8e9cbef4c4a9861
Aozaki Kuro <i@aozaki.cc>
2023-09-28 23:05:03 +0800

    update: head meta

---
+++ components/main/description.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+import Update from '#components/commission/update'
+
+const CommissionDescription = () => {
+  return (
+    <div>
+      <h1 className="">NSFW Commissions</h1>
+      <h2 className="py-6 ss:py-4">Introduction</h2>
+      <p>
+        Preview images are displayed along with corresponding links to platforms like Twitter,
+        Pixiv, and Skeb when available. By clicking these links, you can view the full image. You
+        also have the option to subscribe for updates via <Link href={`/feed.xml`}>RSS</Link>
+      </p>
+      <p className="pt-6">
+        If you enjoy the illustrations, please consider following and supporting the illustrators.
+      </p>
+      <p className="pt-6">
+        For illustrators who are interested in reaching out, feel free to contact me through{' '}
+        <Link href="https://skeb.jp/@Crystallize">skeb</Link>,{' '}
+        <Link href="https://odaibako.net/u/CrystallizeSub">odaibako</Link>, or{' '}
+        <Link href="mailto:skeb@crystallize.eu.org">email</Link>
+      </p>
+      <Update />
+    </div>
+  )
+}
+
+export default CommissionDescription
