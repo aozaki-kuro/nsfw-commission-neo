88a49cc67187ae540f4f3b1f624b7d9b7ce5ac84
Aozaki Kuro <i@aozaki.cc>
2023-10-07 19:53:54 +0800

    optimize: rss generating

--- scripts/rss.ts
+++ scripts/rss.ts
@@ -1,10 +1,9 @@
 import { promises as fs } from 'fs'
-import { commissionData } from '#data/commissionData'
-import { characterDictionary } from '#data/commissionStatus'
 import RSS from 'rss'
 
-import { kebabCase } from '#components/utils'
-import { formatDate } from '#components/utils'
+import { commissionData } from '#data/commissionData'
+import { characterDictionary } from '#data/commissionStatus'
+import { kebabCase, formatDate } from '#components/utils'
 
 // Constants
 const SITE_TITLE = "Crystallize's NSFW Commissions"
@@ -14,15 +13,24 @@ const FEED_URL = `${SITE_URL}/feed.xml`
 const MSG_ERROR = '\x1b[0m[\x1b[31m ERROR \x1b[0m]'
 const MSG_DONE = '\x1b[0m[\x1b[32m DONE \x1b[0m]'
 
-// Mapping from abbreviation to full character name
-const characterNameMapping = Object.fromEntries(
-  characterDictionary.map(({ Abbr, FullName }) => [Abbr, FullName.replace('_', '○')]),
+// Define the shape of the characterNameMapping
+interface CharacterNameMapping {
+  [key: string]: string
+}
+
+// Create the mapping with the defined shape
+const characterNameMapping: CharacterNameMapping = characterDictionary.reduce<CharacterNameMapping>(
+  (acc, { Abbr, FullName }) => {
+    acc[Abbr] = FullName.replace('_', '○')
+    return acc
+  },
+  {},
 )
 
-// Filter for active characters
-const activeCharacters = characterDictionary
-  .filter(char => char.Active === true)
-  .map(char => char.Abbr)
+// Use Set for faster membership checking
+const activeCharacters = new Set(
+  characterDictionary.filter(char => char.Active).map(char => char.Abbr),
+)
 
 function extractDetailsFromFileName(fileName: string) {
   const [datePart, artistPart] = fileName.split('_')
@@ -41,43 +49,36 @@ async function generateRSSFeed() {
       feed_url: FEED_URL,
     })
 
-    // Enhanced sorting and reduced redundant computation
     const sortedCommissions = commissionData
-      // Only include commissions of active characters
-      .filter(commission => activeCharacters.includes(commission.Character))
+      .filter(commission => activeCharacters.has(commission.Character))
       .map(commission => {
         const details = extractDetailsFromFileName(commission.fileName)
+        const characterFullName = characterNameMapping[commission.Character] || commission.Character
         return {
           ...commission,
           ...details,
-          characterFullName: characterNameMapping[commission.Character] || commission.Character,
+          characterFullName,
         }
       })
       .sort((a, b) => b.commissionDate.localeCompare(a.commissionDate))
 
-    for (const {
-      commissionDate,
-      artistName,
-      characterFullName,
-      rawCommissionDate,
-    } of sortedCommissions) {
-      const title = `${characterFullName}`
-      const description = `Illustrator: ${
-        artistName || 'Anonymous'
-      }, published on ${commissionDate}`
-      const link = `${SITE_URL}#${kebabCase(characterFullName)}-${rawCommissionDate}`
+    sortedCommissions.forEach(
+      ({ commissionDate, artistName, characterFullName, rawCommissionDate }) => {
+        feed.item({
+          title: characterFullName,
+          url: `${SITE_URL}#${kebabCase(characterFullName)}-${rawCommissionDate}`,
+          date: new Date(commissionDate),
+          description: `Illustrator: ${artistName || 'Anonymous'}, published on ${commissionDate}`,
+        })
+      },
+    )
 
-      feed.item({
-        title,
-        url: link,
-        date: new Date(commissionDate),
-        description,
-      })
+    try {
+      await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+      console.log(MSG_DONE, 'RSS feed generated successfully!')
+    } catch (writeError) {
+      console.error(MSG_ERROR, 'Error writing RSS feed:', writeError)
     }
-
-    await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
-
-    console.log(MSG_DONE, 'RSS feed generated successfully!')
   } catch (error) {
     console.error(MSG_ERROR, 'Failed to generate RSS feed:', error)
   }
