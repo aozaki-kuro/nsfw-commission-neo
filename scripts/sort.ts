102637153016077d675747ec46885d5171b2316a
Aozaki Kuro <i@aozaki.cc>
2023-10-14 12:24:48 +0800

    feat: sort stale with args

--- scripts/sort.ts
+++ scripts/sort.ts
@@ -1,13 +1,16 @@
-// sort.ts
-
 import { commissionData } from '#data/commissionData' // Adjust the path if necessary
+import { staleData } from '#data/staleData' // Adjust the path if necessary
 import { writeFileSync } from 'fs'
+
+const isStale = process.argv.includes('--stale')
+
+const dataToSort = isStale ? staleData : commissionData
 
 // Function to extract the date from the fileName
 const getDate = (fileName: string): string => fileName.slice(0, 8)
 
-// Sort the commissionData array from latest to oldest
-const sortedCommissionData = commissionData.sort((a, b) => {
+// Sort the data array from latest to oldest
+const sortedData = dataToSort.sort((a, b) => {
   const dateA = getDate(a.fileName)
   const dateB = getDate(b.fileName)
   return dateB.localeCompare(dateA) // Sort in descending order
@@ -16,15 +19,19 @@ const sortedCommissionData = commissionData.sort((a, b) => {
 // Prepare the sorted data for output
 const outputData = `
 import { Props } from "#data/types";
-import { staleData } from "#data/staleData";
+${isStale ? '' : 'import { staleData } from "#data/staleData"'}
 
-export const commissionData: Props[] = ${JSON.stringify(sortedCommissionData, null, 2)
+export const ${isStale ? 'staleData' : 'commissionData'}: Props[] = ${JSON.stringify(
+  sortedData,
+  null,
+  2,
+)
   .replace(/"([^"]+)":/g, '$1:')
   .replace(/\\n/g, '\n')};
 
 `
 
 // Write the sorted data to a new file
-writeFileSync('./data/sortedCommissionData.ts', outputData)
+writeFileSync(`./data/${isStale ? 'sortedStaleData.ts' : 'sortedCommissionData.ts'}`, outputData)
 
-console.log('Sorted data written to sortedCommissionData.ts')
+console.log(`Sorted data written to ${isStale ? 'sortedStaleData.ts' : 'sortedCommissionData.ts'}`)
