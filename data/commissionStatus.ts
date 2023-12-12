1baf1f5884952700759160b6b3de3b504d20d71b
Aozaki Kuro <i@aozaki.cc>
2023-12-12 17:27:54 +0800

    adjust: active status and order

--- data/commissionStatus.ts
+++ data/commissionStatus.ts
@@ -7,15 +7,16 @@ interface Props {
 export const characterDictionary: Props[] = [
   /* Active Characters */
   { DisplayName: 'AZKi', Active: true, Order: 1 },
-  { DisplayName: 'n*yuta', Active: true, Order: 2 },
   { DisplayName: 'Studio K', Active: true, Order: 3 },
   { DisplayName: 'H*nabasami Kyo', Active: true, Order: 5 },
   { DisplayName: 'K*toha', Active: true, Order: 6 }, // TBA
+  { DisplayName: 'Kanaut Nishe', Active: true, Order: 9 },
 
   /* Stale Characters */
-  { DisplayName: 'Blue Archive', Active: false, Order: 99 },
-  { DisplayName: 'L*cia', Active: false, Order: 996 },
-  { DisplayName: "Ninomae Ina'nis", Active: false, Order: 997 }, // Adjusted name as per comment
-  { DisplayName: 'Kanaut Nishe', Active: true, Order: 998 },
+  { DisplayName: 'n*yuta', Active: true, Order: 996 },
+  { DisplayName: 'L*cia', Active: false, Order: 997 },
+  { DisplayName: "Ninomae Ina'nis", Active: false, Order: 998 },
+  { DisplayName: 'Blue Archive', Active: false, Order: 999 },
+
   // { DisplayName: 'Tokomachi', Active: false, Order: 999 },
 ]
