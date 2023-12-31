c3b9b7fd3926012cac361bacec43940f7b99b290
Aozaki Kuro <i@aozaki.cc>
2023-12-31 17:07:14 +0800

    reverse: use sub domain

--- app/components/Analytics.tsx
+++ app/components/Analytics.tsx
@@ -2,19 +2,11 @@ import Script from 'next/script'
 
 const Analytics = () => {
   // Check if process.env.CF_PAGES is 'true'
-  return process.env.CF_PAGES === 'true' ? (
+  return (
     <Script
       data-domain="crystallize.eu.org"
       src="https://sight.crystallize.eu.org/app-event.js"
       strategy="lazyOnload"
-    />
-  ) : (
-    // Return the Script component if the condition is false
-    <Script
-      data-domain="crystallize.eu.org"
-      src="/sight/app.js"
-      strategy="lazyOnload"
-      data-api="/sight/event"
     />
   )
 }
