738af4ace60a2eb7117133b316eae38e2830552d
Aozaki Kuro <i@aozaki.cc>
2024-02-25 20:45:53 +0800

    update: commission and status

--- data/commissionStatus.ts
+++ data/commissionStatus.ts
@@ -7,15 +7,15 @@ interface Props {
 export const characterDictionary: Props[] = [
   /* Active Characters */
   { DisplayName: 'AZKi', Active: true, Order: 1 },
-  { DisplayName: 'Studio K', Active: true, Order: 3 },
+  { DisplayName: 'Studio K', Active: true, Order: 2 },
   { DisplayName: 'H*nabasami Kyo', Active: true, Order: 5 },
   // { DisplayName: 'K*toha', Active: true, Order: 6 }, // TBA
   { DisplayName: 'HACHI', Active: true, Order: 8 },
-  { DisplayName: 'Kanaut Nishe', Active: true, Order: 9 },
 
   /* Stale Characters */
-  { DisplayName: 'n*yuta', Active: false, Order: 996 },
-  { DisplayName: 'L*cia', Active: false, Order: 997 },
+  { DisplayName: 'n*yuta', Active: false, Order: 98 },
+  { DisplayName: 'L*cia', Active: false, Order: 99 },
+  { DisplayName: 'Kanaut Nishe', Active: false, Order: 997 },
   { DisplayName: "Ninomae Ina'nis", Active: false, Order: 998 },
   { DisplayName: 'Blue Archive', Active: false, Order: 999 },
 
