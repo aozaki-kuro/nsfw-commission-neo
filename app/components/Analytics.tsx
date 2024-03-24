d24cd7fc3dee0947b92465177f51565118e91ed3
Aozaki Kuro <i@aozaki.cc>
2024-03-24 19:26:16 +0800

    update: use umami

--- app/components/Analytics.tsx
+++ app/components/Analytics.tsx
@@ -5,10 +5,19 @@ const Analytics = () => {
     return null
   }
 
+  /** Plausible Analytics
   return (
     <Script
       data-domain="crystallize.eu.org"
       src="https://sight.crystallize.eu.org/app-event.js"
+      strategy="lazyOnload"
+    />
+  ) **/
+
+  return (
+    <Script
+      src="https://sight.aozaki.cc/script.js"
+      data-website-id="ae1cfdcf-975a-444b-9f6f-2701b269e54c"
       strategy="lazyOnload"
     />
   )
