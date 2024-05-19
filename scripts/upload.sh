b052d6eec094a493d968c34247ebb85ae00ecb23
Aozaki Kuro <i@aozaki.cc>
2024-05-19 13:29:38 +0800

    fix: share dir

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,7 +2,7 @@
 
 # Define the local and remote directories
 LOCAL_DIR="./data/images/"
-REMOTE_DIR="root@speedypage-jp:./share/nsfw-commission/"
+REMOTE_DIR="root@speedypage-jp:/var/www/share/nsfw-commission/"
 
 # Run rsync to synchronize the directories
 rsync -avc $LOCAL_DIR $REMOTE_DIR
