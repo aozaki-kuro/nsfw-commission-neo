03f9112ad24c766aa78b43b38f0ea783df89a68d
Aozaki Kuro <i@aozaki.cc>
2024-07-26 18:57:29 +0800

    new: commission

--- data/commissionData.ts
+++ data/commissionData.ts
@@ -78,6 +78,10 @@ export const commissionData: Props = [
     Character: 'Studio K',
     Commissions: [
       {
+        fileName: '20240726_Welcome party', // CIEL w/ V.W.P
+        Links: [''],
+      },
+      {
         fileName: '20240513', // CIEL
         Links: ['https://www.pixiv.net/artworks/118703993'],
       },
