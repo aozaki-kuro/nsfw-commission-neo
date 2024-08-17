85216942d63b894542d6fffb04d5671f3e6a0f04
Aozaki Kuro <i@aozaki.cc>
2024-08-17 12:22:31 +0800

    chore: use mjs for prettier settings

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,4 +1,9 @@
-module.exports = {
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+const config = {
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
@@ -6,3 +11,5 @@ module.exports = {
   printWidth: 100,
   plugins: ['prettier-plugin-tailwindcss'],
 }
+
+export default config
