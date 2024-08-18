1451e59a856ebf20aa419a9ff973ca4f83d4e973
Aozaki Kuro <i@aozaki.cc>
2024-08-18 22:38:16 +0800

    fix: broken cover img link

--- app/components/main/siteMeta.ts
+++ app/components/main/siteMeta.ts
@@ -2,7 +2,7 @@ import { Metadata } from 'next'
 
 const Site = 'NSFW Commission'
 const Description = 'The collection of commissioned NSFW illustrations / Do Not Repost'
-const twitterCard = `https://img.crystallize.cc/nsfw-cover.jpg`
+const twitterCard = `https://img.crystallize.cc/nsfw-cover-s.webp`
 const CanonicalUrl = 'https://crystallize.cc'
 
 export const SiteMeta: Metadata = {
