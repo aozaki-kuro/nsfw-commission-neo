a4d3798e7bb1cc97da70de38b04d8e905f6b50d6
Aozaki Kuro <i@aozaki.cc>
2024-10-06 11:38:15 +0800

    update: display 3 latest

--- app/components/main/Update.tsx
+++ app/components/main/Update.tsx
@@ -3,40 +3,41 @@ import { commissionData } from '#data/commissionData'
 import Link from 'next/link'
 
 // Calculate the total number of commissions
-const totalCommissions = commissionData.reduce((count, data) => {
-  return count + data.Commissions.length
-}, 0)
+const totalCommissions = commissionData.reduce((count, data) => count + data.Commissions.length, 0)
 
-const latestEntry = commissionData
+const latestEntries = commissionData
   .filter(data => isCharacterActive(data.Character))
   .flatMap(data =>
     data.Commissions.map(commission => ({ ...commission, Character: data.Character })),
   )
-  .sort((a, b) => b.fileName.localeCompare(a.fileName))[0]
+  .sort((a, b) => b.fileName.localeCompare(a.fileName)) // Sort by filename in descending order
+  .slice(0, 3) // Get the latest 3 entries
 
-const Update = () => {
-  if (!latestEntry) return <p className="font-mono text-sm">No active updates found</p>
-
-  const { fileName, Character } = latestEntry
-
-  return (
+const Update = () =>
+  latestEntries.length === 0 ? (
+    <p className="font-mono text-sm">No active updates found</p>
+  ) : (
     <div className="flex flex-col pb-4 pt-8 font-mono text-sm ss:text-xs md:pt-6">
-      {/* Total commissions on its own line */}
       <p className="pb-2">Currently {totalCommissions} commissions</p>
 
-      {/* Last update info on a single line */}
-      <p className="pr-2">
-        Last update: {formatDate(fileName.substring(0, 8))} {'[ '}
-        <Link
-          href={`#${kebabCase(Character)}-${fileName.substring(0, 8)}`}
-          className="plausible-event-name=Check+Latest underline-offset-[0.1rem]"
-        >
-          {Character}
-        </Link>
-        {' ]'}
-      </p>
+      <div className="flex items-start">
+        <p className="pr-2">Last update:</p>
+        <div className="flex flex-col space-y-2">
+          {latestEntries.map(({ fileName, Character }, index) => (
+            <p key={index} className="mb-2 pr-2">
+              {formatDate(fileName.substring(0, 8))} {'[ '}
+              <Link
+                href={`#${kebabCase(Character)}-${fileName.substring(0, 8)}`}
+                className="underline-offset-[0.1rem]"
+              >
+                {Character}
+              </Link>
+              {' ]'}
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
   )
-}
 
 export default Update
