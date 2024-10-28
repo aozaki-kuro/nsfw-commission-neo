e57fa32323281544990e85eb242bc27d2865d083
Aozaki Kuro <i@aozaki.cc>
2024-10-28 20:11:54 +0800

    fix: remove unsed logic

--- app/components/commission/Listing.tsx
+++ app/components/commission/Listing.tsx
@@ -36,11 +36,9 @@ const Listing = ({ Character }: { Character: string }) => {
   const characterData = commissionData.find(data => data.Character === Character)
 
   // Sort commissions in descending order, filter out hidden commissions
-  const sortedCommissions = [...(characterData?.Commissions || [])]
-    .filter(commission => !commission.hidden) // Filter out hidden commissions
-    .sort((a, b) => {
-      return Number(b.fileName.slice(0, 8)) - Number(a.fileName.slice(0, 8))
-    })
+  const sortedCommissions = [...(characterData?.Commissions || [])].sort((a, b) => {
+    return Number(b.fileName.slice(0, 8)) - Number(a.fileName.slice(0, 8))
+  })
 
   return (
     <div className="pb-6">
