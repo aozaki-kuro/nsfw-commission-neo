1e19b025d7404c57efe4c31cae43be9d75057581
Aozaki Kuro <i@aozaki.cc>
2024-12-09 18:09:34 +0800

    fix: update description

--- data/Active/Nayuta.ts
+++ data/Active/Nayuta.ts
@@ -9,7 +9,7 @@ export const Nayuta: Props = [
         fileName: '20241208',
         Links: ['https://www.pixiv.net/artworks/125007277'],
         // Design: 'https://twitter.com/7utauta/status/1839664172331864348',
-        // Description: '17th Album Release',
+        Description: '17th Album Release',
       },
       {
         fileName: '20231031_七市',
