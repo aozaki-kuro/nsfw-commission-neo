b25a84cf3a831d49b61d086241b2c434783bfd50
Aozaki Kuro <i@aozaki.cc>
2024-12-22 10:05:40 +0800

    chore: minor cleanup

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,11 @@
 import { FlatCompat } from '@eslint/eslintrc'
-import path from 'path'
+import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 import prettierConfigRecommended from 'eslint-plugin-prettier/recommended'
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
