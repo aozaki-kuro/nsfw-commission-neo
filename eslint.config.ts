e73d98bdd1aa51e17df988bdfc7312c9384d7cb6
Aozaki Kuro <i@aozaki.cc>
2024-12-22 10:03:17 +0800

    fix: sync with latest eslint config from example

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,26 +1,18 @@
-import js from '@eslint/js'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-import { fixupConfigRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import prettierConfigRecommended from 'eslint-plugin-prettier/recommended'
-import ts from 'typescript-eslint'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 })
 
-const patchedConfig = fixupConfigRules([...compat.extends('next/core-web-vitals')])
-
-const config = [
-  ...patchedConfig,
-  ...ts.configs.recommended,
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   prettierConfigRecommended,
   {
     ignores: ['**/.next/**', '**/_next/**', '**/dist/**', '**/out/**'],
@@ -34,4 +26,4 @@ const config = [
   },
 ]
 
-export default config
+export default eslintConfig
