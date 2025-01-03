88b3d8ee19c09689b7230b86b31bfa2fcd32cc75
Aozaki <i@aozaki.cc>
2025-01-03 19:41:33 +0800

    chore: minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,13 @@ const Home: NextPage = () => {
   return (
     <RootLayout>
       <Warning />
-      <div className="mx-auto">
-        <div className="relative flex justify-center">
-          <div id="Main Contents" className="w-full max-w-[40rem]">
-            <CommissionDescription />
-            <Commission />
-            <Footer />
-          </div>
-          <CharacterList />
+      <div className="relative mx-auto flex justify-center">
+        <div id="Main Contents" className="w-full max-w-[40rem]">
+          <CommissionDescription />
+          <Commission />
+          <Footer />
         </div>
+        <CharacterList />
       </div>
       <Hamburger />
     </RootLayout>
