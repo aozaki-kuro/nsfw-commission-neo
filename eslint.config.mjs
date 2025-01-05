32b5675aab5ca759a50a15d98c1470e0f7317423
Aozaki <i@aozaki.cc>
2025-01-05 16:48:37 +0800

    chore: use mjs instead of ts for eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,9 @@
 import { FlatCompat } from '@eslint/eslintrc'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
 
 import prettierConfigRecommended from 'eslint-plugin-prettier/recommended'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 })
 
 const eslintConfig = [
